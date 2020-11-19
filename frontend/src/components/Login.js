c1c3b81783653dcad3c4d8a8b8cd4d4f189ea402
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-19 08:16:34 -0500

    update

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -66,7 +66,7 @@ function Login()
             </form>
             <span id="loginResult">{message}</span>
         <div id="formFooter">
-            <a class="underlineHover onClick={doForgetPassword}>Forgot Password?</a>
+            <q class="underlineHover onClick={doForgetPassword}>Forgot Password?</q>
     	</div>
 
         </div>
