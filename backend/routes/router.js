b857d494ea2704b2a0e7796334d4409e074becb6
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-19 22:04:39 -0500

    updating branch

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -10,10 +10,20 @@ router.get('/auth/refresh', AuthController.refresh);
 
 router.delete('/auth/logout', AuthController.logout);
 
+router.get('/auth/activate/:code', AuthController.activate);
+
+router.post('/auth/forgotpass', AuthController.forgotpass);
+
+/* 
+The endpoints below require JWT verification.
+*/
+
 router.get('/protected', Middleware.verify, (req, res) => {
     res.setHeader('content-type', 'application/json; charset=utf-8')
     return res.status(200).json({userName: req.user.userName,
                                  id: req.user.id});
 })
+
+router.post('/auth/changepass', Middleware.verify, AuthController.changepass);
 
 module.exports = router;
