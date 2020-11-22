38caab2f2f4ed7dccda0da42c3b654373553e80a
smcchargs <52867301+smcchargs@users.noreply.github.com>
2020-11-22 14:22:17 -0500

    Added in the submit availability page and changed card styling (#34)

--- mobileApp/src/Card.js
+++ mobileApp/src/Card.js
@@ -13,11 +13,16 @@ export default function Card(props) {
   return (
     <View style={style.card}> 
       <View style={style.side}/>
-      <Text style={style.text}>
-        {props.name + '\n'}
-        {'Start Time: ' + props.start + '\n'}
-        {'End Time: ' + props.end + '\n'}
-      </Text>
+      <View style={style.text}>
+        <Text style={style.textName}>
+          {props.name}
+        </Text>
+
+            <Text style={style.textTime}>
+            {'Start Time: ' + props.start + '\n'}
+            {'End Time: ' + props.end + '\n'}
+            </Text>
+      </View>
     </View>
   );
 }
@@ -28,19 +33,31 @@ const style = StyleSheet.create({
     width: "100%",
     height: 150,
     borderRadius: 15,
-    margin: 5,
-    padding: 20,
+    margin: 0,
+    paddingLeft: 0,
+    paddingBottom: 2,
     flexDirection: 'row'
   },
   text: {
+    flexDirection: "column",
+    paddingRight: 100
+  },
+  textName: {
     fontWeight: "bold",
     color: "black",
     textAlign: "left",
     fontSize: 25,
-    
+    paddingLeft: 5,
+  },
+  textTime: {
+    color: "black",
+    textAlign: "left",
+    fontSize: 20,
+    paddingLeft: 5,
+    paddingTop: 0,
   },
   side: {
     padding: 5,
     backgroundColor: "lightgrey",
   }
-});
+}); 
