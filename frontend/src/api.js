4c1cd03c219e62e4c9e0ad4a7bbe4190fac98c4f
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-23 10:48:37 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,70 +1,71 @@
-import axios from "axios"
+import axios from "axios";
 axios.defaults.withCredentials = true;
 
 var url;
-if (process.env.NODE_ENV === 'production') {
-    url = 'https://' + process.env.REACT_APP_NAME +  '.herokuapp.com/api';
-}
-else {        
-    url = 'http://localhost:5000/api';
+if (process.env.NODE_ENV === "production") {
+  url = "https://" + process.env.REACT_APP_NAME + ".herokuapp.com/api";
+} else {
+  url = "http://localhost:5000/api";
 }
 
 axios.interceptors.request.use(
-    (config) => {
-        const accessToken = localStorage.getItem('accessToken');
-        if (accessToken) {
-            config.headers['x-auth-token'] = accessToken;
-        }
-        config.headers['content-type'] = 'application/json; charset=utf-8'
-        return config;
-    },
-    (error) => {
-        Promise.reject(error);
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers["x-auth-token"] = accessToken;
     }
+    config.headers["content-type"] = "application/json; charset=utf-8";
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
 );
 
 axios.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    function (error) {
-        const originalRequest = error.config;
+  (response) => {
+    return response;
+  },
+  function (error) {
+    const originalRequest = error.config;
 
-        if (
-            error.response.status === 401 &&
-            !originalRequest._retry
-        ) {
-            originalRequest._retry = true;
-            return axios.get(`${url}/auth/refresh`)
-            .then((res) => {
-                if (res.status === 200) {
-                    let accessToken = JSON.stringify(res.data.accessToken);
-                    localStorage.setItem('accessToken', accessToken);
-                    console.log('Refreshed access token.')
-                    return axios(originalRequest);
-                }
-            });
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      return axios.get(`${url}/auth/refresh`).then((res) => {
+        if (res.status === 200) {
+          let accessToken = JSON.stringify(res.data.accessToken);
+          localStorage.setItem("accessToken", accessToken);
+          console.log("Refreshed access token.");
+          return axios(originalRequest);
         }
-        return Promise.reject(error);
+      });
     }
-)
+    return Promise.reject(error);
+  }
+);
 
 const api = {
-    signup: (body) => {
-        return axios.post(`${url}/auth/signup`, body);
-    },
-    login: (body) => {
-        return axios.post(`${url}/auth/login`, body);
-    },
-    refresh: () => {
-        return axios.get(`${url}/auth/refresh`);
-    },
-    logout: (body) => {
-        return axios.delete(`${url}/auth/logout`, {data: body});
-    },
-    protected: () => {
-        return axios.get(`${url}/protected`);
-    }
+  signup: (body) => {
+    return axios.post(`${url}/auth/signup`, body);
+  },
+  changePassword: (body) => {
+    return axios.post(`${url}/auth/changepass`, body);
+  },
+  forgotPassword: (body) => {
+    return axios.post(`${url}/auth/forgotpass`, body);
+  },
+  login: (body) => {
+    return axios.post(`${url}/auth/login`, body);
+  },
+  refresh: () => {
+    return axios.get(`${url}/auth/refresh`);
+  },
+  logout: (body) => {
+    return axios.delete(`${url}/auth/logout`, { data: body });
+  },
+  protected: () => {
+    return axios.get(`${url}/protected`);
+  },
 };
 
 export default api;
