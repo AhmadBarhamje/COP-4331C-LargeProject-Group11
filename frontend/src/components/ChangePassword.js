8f2652ecbea35c98f0af83704ba608dc9c77217c
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-23 10:37:23 -0500

    Update ChangePassword.js

--- frontend/src/components/ChangePassword.js
+++ frontend/src/components/ChangePassword.js
@@ -1,30 +1,69 @@
-import React, {useState} from 'react';
-import api from '../api';
+import React, { useState } from "react";
 
-function ForgotPassword()
-{
-    var CurrentPassword;
-    var NewPassword;
-    
-    const [message,setMessage] = useState('');
+import api from "../api";
 
-  
-    };
+function ChangePassword() {
+  var CurrentPassword;
+  var NewPassword;
 
-
-    return(
-	<div class="wrapper fadeInDown">
-        <div id="loginDiv">
-            <span id="inner-title">Change Password</span><br />
-	    <form onSubmit={doForgotpass}>
-            <input type="text" id="loginName" class="fadeIn second" placeholder="Current Password" 
-                ref={(c) => CurrentPassword = c} /><br />			    
-	    <input type="text" id="Email" class="fadeIn second" placeholder="New Password"/><br />          
-                ref={(c) => NewPassword = c} /><br />	    
-            <input type="submit" id="loginButton" className="buttons" value = "Reset Password"/>
-            </form>
-
-        </div>
-	</div>
-    );
-};
+  const [message, setMessage] = useState("");
+  const [password, setPassword] = useState("");
+  const [newPassword, setnewPassword] = useState("");
+  //onSubmit={doForgotpass}
+  const changePasswordCaller = async (event) => {
+    event.preventDefault();
+    console.log("access token", localStorage.getItem("accessToken"));
+    var obj = { password: password, newPassword: newPassword };
+    let res;
+    try {
+      res = await api.changePassword(obj);
+      console.log("res from change", res);
+      if (res.data.success) {
+        alert("password Changed Successfully");
+      } else {
+        alert(res.data.error);
+      }
+    } catch (e) {
+      alert(e.toString());
+      return;
+    }
+  };
+  return (
+    <div class='wrapper fadeInDown'>
+      <div id='loginDiv'>
+        <span id='inner-title'>Change Password</span>
+        <br />
+        <form>
+          <input
+            type='text'
+            id='loginName'
+            class='fadeIn second'
+            placeholder='Current Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <input
+            type='text'
+            id='Email'
+            value={newPassword}
+            onChange={(e) => setnewPassword(e.target.value)}
+            class='fadeIn second'
+            placeholder='New Password'
+          />
+          <br />
+          {/* ref={(c) => (NewPassword = c)} /> */}
+          <br />
+          <input
+            type='submit'
+            id='loginButton'
+            onClick={changePasswordCaller}
+            className='buttons'
+            value='Reset Password'
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+export default ChangePassword;
