74bc8ef0712b9ce7af7700590910bb9d21f84393
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-23 10:49:54 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import "./App.css";
 
-import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ForgotPasswordPage from './pages/ChangePasswordPage';
+import LoginPage from "./pages/LoginPage";
+import CardPage from "./pages/CardPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
-          <LoginPage/>
+        <Route path='/' exact>
+          <LoginPage />
         </Route>
-        <Route path="/forgotpassword" exact>
-          <ForgotPasswordPage/>
+        <Route path='/forgotpassword' exact>
+          <ForgotPasswordPage />
         </Route>
-	<Route path="/changepassword" exact>
-          <ChangePasswordPage/>
+        <Route path='/changepassword' exact>
+          <ChangePasswordPage />
         </Route>
-        <Route path="/cards" exact>
-          <CardPage/>
+        <Route path='/cards' exact>
+          <CardPage />
         </Route>
-        <Redirect to="/"/>
+        <Redirect to='/' />
       </Switch>
     </Router>
-    );
+  );
 }
 
 export default App;
