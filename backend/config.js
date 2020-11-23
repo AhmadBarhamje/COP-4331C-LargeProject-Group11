eb578e41ad69926a144e887bdea40b4b4f6392be
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-23 10:35:33 -0500

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+exports.MONGODB_URI =
+  "mongodb+srv://Shawn:1234@schedulercluster.uycpg.mongodb.net/Scheduler?retryWrites=true&w=majority";
+exports.USER = "naas.christopher1@gmail.com";
+exports.PASS = "1NWGQOpgaCsX5MY4";
+exports.ACCESS_TOKEN_SECRET = "thisisaverysecretstring";
+exports.ACCESS_TOKEN_LIFE = "1m";
+exports.REFRESH_TOKEN_SECRET = "thisisalsoaverysecretstring";
+exports.REFRESH_TOKEN_LIFE = "12h";
+exports.ORIGIN = "http://localhost:3000";
