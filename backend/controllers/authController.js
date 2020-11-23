0ab88a64a10a273f478676d6d5d55a8ae9122864
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-23 13:26:22 -0500

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,105 +1,109 @@
-const User = require('../models/user.model')
-const Token = require('../models/token.model')
-const Avail = require('../models/availability.model')
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const emailer = require('./emailer')
-const {ACCESS_TOKEN_SECRET, ACCESS_TOKEN_LIFE, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_LIFE, ORIGIN} = process.env;
-var generator = require('generate-password');
-const { use } = require('../routes/router')
+const User = require("../models/user.model");
+const Token = require("../models/token.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const emailer = require("./emailer");
+const {
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_LIFE,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_LIFE,
+  ORIGIN,
+} = require("./../config");
+var generator = require("generate-password");
 
 exports.signup = async (req, res) => {
-    try {
-        //check if username is already taken:
-        var user
-        user = await User.findOne({ userName: req.body.userName });
-        if (user) {
-            return res.status(200).json({ error: "Username taken." , id: -3 });
-        } 
-        // check if email taken
-        user = await User.findOne({ email: req.body.email });
-        if (user) {
-            return res.status(200).json({ error: "Email in use." , id: -4 });
-        }
+  try {
+    //check if username is already taken:
+    var user;
+    user = await User.findOne({ userName: req.body.userName });
+    if (user) {
+      return res.status(400).json({ error: "Username taken." });
+    }
+    // check if email taken
+    user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Email in use." });
+    }
 
-        //create new user and generate a pair of tokens and send
-        // user = await new User(req.body).save();
-        var code = getRandomCode();
-        console.log(code);
-        user = await new User({email: req.body.email,
-                            userName: req.body.userName,
-                            password: req.body.password,
-                            firstName: req.body.firstName,
-                            lastName: req.body.lastName,
-                            activationCode: code})
-                            .save()
+    //create new user and generate a pair of tokens and send
+    // user = await new User(req.body).save();
+    var code = getRandomCode();
+    console.log(code);
+    user = await new User({
+      email: req.body.email,
+      userName: req.body.userName,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      activationCode: code,
+    }).save();
 
-        emailer.sendRegistrationEmail(user.email, code);
-    
-        return res.status(201).json({ error: "" });
-    } catch (e) {
-        console.error(e);
-        return res.status(500).json({ e: "Internal Server Error!" });
+    emailer.sendRegistrationEmail(user.email, code);
+
+    return res.status(201).json({ error: "" });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ e: "Internal Server Error!" });
   }
 };
 
-
 function getRandomCode() {
-    min = 1000000;
-    max = 9999999;
-    return Math.floor(Math.random() * (max-min) + min);
+  min = 1000000;
+  max = 9999999;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 exports.activate = async (req, res) => {
-    try {
-        let code = req.params.code;
-        let user = await User.findOne({ activationCode: code });
-        if (!user) {
-            return res.status(404).json({ error: "Cannot find user"})
-        } else {
-            await user.updateOne({$unset: {activationCode: ""}, active: true});
-            await new Avail({userId: user._id, userName: user.userName}).save();
-            return res.status(200).redirect(process.env.ORIGIN);
-        }
-    } catch (e) {
-        console.error(e);
-        return res.status(500).redirect(process.env.ORIGIN);
+  try {
+    let code = req.params.code;
+    let user = await User.findOne({ activationCode: code });
+    if (!user) {
+      return res.status(404).json({ error: "Cannot find user" });
+    } else {
+      await user.updateOne({ $unset: { activationCode: "" }, active: true });
+      return res.status(200).redirect(ORIGIN);
     }
-}
+  } catch (e) {
+    console.error(e);
+    return res.status(500).redirect(ORIGIN);
+  }
+};
 
 exports.login = async (req, res) => {
-    try {
-        //check if user exists in database:
-        let user = await User.findOne({ userName: req.body.userName });
-        //send error if no user found:
-        if (!user) {
-            console.log('User not found!');
-            return res.status(200).json({ id: -1 });
-        } else {
-            //check if password is valid:
-            let valid = await bcrypt.compare(req.body.password, user.password);
-            if (valid) {
+  try {
+    //check if user exists in database:
+    let user = await User.findOne({ userName: req.body.userName });
+    //send error if no user found:
+    if (!user) {
+      console.log("User not found!");
+      return res.status(200).json({ id: -1 });
+    } else {
+      //check if password is valid:
+      let valid = await bcrypt.compare(req.body.password, user.password);
+      if (valid) {
+        // Check if account is active
+        if (!user.active) {
+          console.log("Email not verified!");
+          return res.status(200).json({ id: -2 });
+        }
 
-                // Check if account is active
-                if (!user.active) {
-                    console.log('Email not verified!');
-                    return res.status(200).json({ id: -2 });
-                }
-
-                //generate a pair of tokens if valid and send
-                let accessToken = await user.createAccessToken();
-                let refreshToken = await user.createRefreshToken();
-                let id = user._id;
-                let firstName = user.firstName;
-                let lastName = user.lastName;
-                let userName = user.userName;
-                res.cookie('refreshToken', refreshToken, {httpOnly: true});
-                return res.status(200).json({ accessToken, id, firstName, lastName, userName});
-            } else {
-                //send error if password is invalid
-                console.log('Invalid password!');
-                return res.status(200).json({ id: -1 });
-            }
+        //generate a pair of tokens if valid and send
+        let accessToken = await user.createAccessToken();
+        let refreshToken = await user.createRefreshToken();
+        let id = user._id;
+        let firstName = user.firstName;
+        let lastName = user.lastName;
+        let userName = user.userName;
+        res.cookie("refreshToken", refreshToken, { httpOnly: true });
+        return res
+          .status(200)
+          .json({ accessToken, id, firstName, lastName, userName });
+      } else {
+        //send error if password is invalid
+        console.log("Invalid password!");
+        return res.status(200).json({ id: -1 });
+      }
     }
   } catch (e) {
     console.error(e);
@@ -140,54 +144,57 @@ exports.refresh = async (req, res) => {
 };
 
 exports.logout = async (req, res) => {
-    try {
-        //delete the refresh token saved in database:
-        const { userId } = req.body;
-        await Token.deleteMany({userId: userId});
-        return res.status(200).json({ success: true });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal Server Error!" });
-    }
+  try {
+    //delete the refresh token saved in database:
+    const { userId } = req.body;
+    await Token.deleteMany({ userId: userId });
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error!" });
+  }
 };
 
 exports.forgotpass = async (req, res) => {
-    try {
-        const { userName, email } = req.body;
-        var user = await User.findOne( {userName: userName, email: email} );
-        if (!user) {
-            return res.status(200).json( {success: true, error: ""} )
-        } else {
-            tempPassword = generator.generate({numbers: true, strict: true});
-            console.log(`Temporary password ${tempPassword}`);
-            user.password = tempPassword;
-            await user.save();
+  try {
+    const { userName, email } = req.body;
+    var user = await User.findOne({ userName: userName, email: email });
+    if (!user) {
+      return res
+        .status(200)
+        .json({ success: false, error: "Username or email is wrong" });
+    } else {
+      tempPassword = generator.generate({ numbers: true, strict: true });
+      console.log(`Temporary password ${tempPassword}`);
+      user.password = tempPassword;
+      await user.save();
 
-            emailer.forgotPasswordEmail(user.email, tempPassword);
-            return res.status(200).json( {success: true, error: ""} )
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal Server Error!" })
+      emailer.forgotPasswordEmail(user.email, tempPassword);
+      return res.status(200).json({ success: true, error: "" });
     }
-}
-
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error!" });
+  }
+};
 
 exports.changepass = async (req, res, next) => {
-    try {
-        let {id, userName} = req.user;
-        const {password, newPassword} = req.body;
-        var user = await User.findOne({ _id: id });
-        let valid = await bcrypt.compare(password, user.password);
-        if (valid) {
-            user.password = newPassword;
-            await user.save();
-            return res.status(200).json({success: true, error: ""});
-        } else {
-            return res.status(200).json({success: false, error: "Old password is incorrect"})
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal Server Error!" });
+  try {
+    let { id, userName } = req.user;
+    const { password, newPassword } = req.body;
+    var user = await User.findOne({ _id: id });
+    let valid = await bcrypt.compare(password, user.password);
+    if (valid) {
+      user.password = newPassword;
+      await user.save();
+      return res.status(200).json({ success: true, error: "" });
+    } else {
+      return res
+        .status(200)
+        .json({ success: false, error: "Old password is incorrect" });
     }
-}
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error!" });
+  }
+};
