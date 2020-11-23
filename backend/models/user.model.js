80c3aa996ccc2e78f566f619ba8279fdd7730c57
samsannchez <samanthasanchez@knights.ucf.edu>
2020-11-23 13:28:52 -0500

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -10,45 +10,46 @@ const {
 } = require("./../config");
 
 const userSchema = new mongoose.Schema({
-
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    userName: {type: String, required: true},
-    active: {type: Boolean, default: false, required: true},
-    activationCode: {type: Number}
-})
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  userName: { type: String, required: true },
+  active: { type: Boolean, default: false },
+  activationCode: { type: Number },
+});
 
 userSchema.methods = {
-    createAccessToken: async function() {
-        try {
-            let {_id, userName} = this;
-            let accessToken = jwt.sign(
-                {user: {_id, userName}},
-                ACCESS_TOKEN_SECRET, {expiresIn: ACCESS_TOKEN_LIFE}
-            );
+  createAccessToken: async function () {
+    try {
+      let { _id, userName } = this;
+      let accessToken = jwt.sign(
+        { user: { _id, userName } },
+        ACCESS_TOKEN_SECRET,
+        { expiresIn: ACCESS_TOKEN_LIFE }
+      );
 
-            return accessToken;
-        } catch(e) {
-            console.error(e);
-            return;
-        }
-    },
-    createRefreshToken: async function() {
-        try{
-            let {_id, userName} = this;
-            let refreshToken = jwt.sign(
-                {user: {_id, userName}},
-                REFRESH_TOKEN_SECRET, {expiresIn: REFRESH_TOKEN_LIFE}
-            );
-            await Token.deleteMany({userId: _id});
-            await new Token({token: refreshToken, userId: _id}).save();
-            return refreshToken;
-        } catch(e) {
-            console.error(e);
-            return;
-        }
+      return accessToken;
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+  },
+  createRefreshToken: async function () {
+    try {
+      let { _id, userName } = this;
+      let refreshToken = jwt.sign(
+        { user: { _id, userName } },
+        REFRESH_TOKEN_SECRET,
+        { expiresIn: REFRESH_TOKEN_LIFE }
+      );
+      await Token.deleteMany({ userId: _id });
+      await new Token({ token: refreshToken, userId: _id }).save();
+      return refreshToken;
+    } catch (e) {
+      console.error(e);
+      return;
+    }
   },
 };
 
