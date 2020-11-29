8a11eef966f7e15d600b986c63ddcf6388ad5f74
samsannchez <69266761+samsannchez@users.noreply.github.com>
2020-11-29 09:51:59 -0500

    Update LoggedInName.js

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -23,7 +23,17 @@ function LoggedInName()
     };    
 
   return(
-   <div id="loggedInDiv fadeIn fourth topbanner">
+   <div id="loggedInDiv fadeIn fourth" style="  -webkit-border-radius: 10px 10px 10px 10px;
+  border-radius: 10px 10px 10px 10px;
+  background-color: #fff;
+  height: 60px;
+  width: 100%;
+  max-width: 1500px;
+  position: relative;
+  padding-left:10px;
+  -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+  box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+  text-align: left;">
    <span id="userName">Logged In As {firstName} {lastName}</span>
    <button type="button" id="logoutButton" class="buttons" 
      onClick={doLogout}> Log Out </button>
