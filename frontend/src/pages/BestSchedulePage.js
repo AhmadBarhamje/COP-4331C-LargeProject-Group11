532fdd090504bd8fd360b40fa6b7df0d56f20e52
samsannchez <69266761+samsannchez@users.noreply.github.com>
2020-12-01 20:30:45 -0500

    Create BestSchedulePage.js

---
+++ frontend/src/pages/BestSchedulePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import BestSchedule from "../components/BestSchedule";
+
+const BestSchedulePage = () => {
+  return <BestSchedule />;
+};
+
+export default BestSchedulePage;
