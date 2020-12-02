0a3083d8dad173581ce15645340c13ed8d84d124
Israel Soria <israel.soria545@knights.ucf.edu>
2020-12-02 03:34:37 -0500

    react-table

--- frontend/src/components/BasicTable.js
+++ frontend/src/components/BasicTable.js
@@ -1,18 +1,107 @@
 import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
+import { useTable, useRowSelect } from 'react-table'
 import {COLUMNS} from './columns';
 import MOCK_DATA from './MOCK_DATA.json'
 import api from '../api';
 import './table.css';
 
+// const IndeterminateCheckbox = React.forwardRef(
+//     ({ indeterminate, ...rest }, ref) => {
+//       const defaultRef = React.useRef()
+//       const resolvedRef = ref || defaultRef
+  
+//       React.useEffect(() => {
+//         resolvedRef.current.indeterminate = indeterminate
+//       }, [resolvedRef, indeterminate])
+  
+//       return (
+//         <>
+//           <input type="checkbox" ref={resolvedRef} {...rest} />
+//         </>
+//       )
+//     }
+//   )
+
+var sendToAPI = {
+     newAvailability :
+     {
+         sun: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
+         mon: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
+         tue: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
+         wed: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
+         thu: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
+         fri: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
+         sat: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
+     }
+}
+
+const doRefresh = async event =>
+{
+    event.preventDefault();
+    let res
+    res = await api.getAvailability();
+    console.log(sendToAPI.newAvailability);
+    console.log(res.data.availability);
+    sendToAPI.newAvailability = res.data.availability; // idk which to use tbh
+    // sendToAPI.newAvailability.sun = res.data.availability.sun; // when i use this one the global isn't normal
+    // sendToAPI.newAvailability.mon = res.data.availability.mon; // it defaults to the api value 
+    // sendToAPI.newAvailability.tue = res.data.availability.tue;
+    // sendToAPI.newAvailability.wed = res.data.availability.wed;
+    // sendToAPI.newAvailability.thu = res.data.availability.thu;
+    // sendToAPI.newAvailability.fri = res.data.availability.fri;
+    // sendToAPI.newAvailability.sat = res.data.availability.sat;
+    MOCK_DATA.concat(sendToAPI);
+    console.log(sendToAPI.newAvailability);
+    //this.updateData();
+}
+
 export const BasicTable = () => {
+
+    const EditableCell = ({
+        value: initialValue,
+        row: { index },
+        column: { id },
+        updateMyData, // This is a custom function that we supplied to our table instance
+      }) => {
+        // We need to keep and update the state of the cell normally
+        const [value, setValue] = React.useState(initialValue)
+      
+        const onClick = e => {
+          setValue(e.target.value = 1 - e.target.value);
+          updateData(index, id, e.target.value);
+        }
+
+        React.useEffect(() => {
+            setValue(initialValue)
+          }, [initialValue])
+      
+        return <input value={value} onClick={onClick} />
+    }
+    
+    function updateData(row, col, val) {
+        // Update the correct value mapped in the JSON we plan on sending here
+        console.log(sendToAPI.newAvailability);
+        let temp = sendToAPI.newAvailability;
+        console.log(`row: ${row} col: ${col}`)
+        Object.values(temp)[row][col] = parseInt(val);
+        console.log(sendToAPI);
+        return;
+    }
+
+    const defaultColumn = {
+        Cell: EditableCell,
+    }
+
     const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(() => MOCK_DATA, [])
+    var data = useMemo(() => MOCK_DATA, [])
 
     const tableInstance = useTable({
         columns,
-        data
-    })
+        data,
+        defaultColumn
+    }
+    )
+
 
     const { 
         getTableProps, 
@@ -23,6 +112,8 @@ export const BasicTable = () => {
     } = tableInstance
 
     return (
+        <>
+        <button onClick={doRefresh}>Refresh</button>
         <table {...getTableProps()}>
             <thead>
                 {headerGroups.map((headerGroups) => ( 
@@ -46,5 +137,6 @@ export const BasicTable = () => {
                     })}
             </tbody>
         </table>
+        </>
     )
 }
--- frontend/src/components/columns.js
+++ frontend/src/components/columns.js
@@ -2,34 +2,198 @@ import ReactTable from "react-table";
 export const COLUMNS = [
     {
         Header: 'Time',
-        Column: "00.00"
+        accessor: 'day'
     },
     {
-        Header: 'Sunday',
-        accessor: 'sun'
+        Header: '00:00 - 00:30',
+        accessor: '0'
     },
     {
-        Header: 'Monday',
-        accessor: 'mon'
+        Header: '00:30 - 01:00',
+        accessor: '1'
     },
     {
-        Header: 'Tuesday',
-        accessor: 'tue'
+        Header: '01:00 - 01:30',
+        accessor: '2'
     },
     {
-        Header: 'Wednesday',
-        accessor: 'wed'
+        Header: '01:30 - 02:00',
+        accessor: '3'
     },
     {
-        Header: 'Thursday',
-        accessor: 'thu'
+        Header: '02:00 - 02:30',
+        accessor: '4'
     },
     {
-        Header: 'Friday',
-        accessor: 'fri'
+        Header: '02:30 - 03:00',
+        accessor: '5'
     },
     {
-        Header: 'Saturday',
-        accessor: 'sat'
+        Header: '03:00 - 03:30',
+        accessor: '6'
+    },
+    {
+        Header: '03:30 - 04:00',
+        accessor: '7'
+    },
+    {
+        Header: '04:00 - 04:30',
+        accessor: '8'
+    },
+    {
+        Header: '04:30 - 05:00',
+        accessor: '9'
+    },
+    {
+        Header: '05:00 - 05:30',
+        accessor: '10'
+    },
+    {
+        Header: '05:30 - 06:00',
+        accessor: '11'
+    },
+    {
+        Header: '06:00 - 06:30',
+        accessor: '12'
+    },
+    {
+        Header: '06:30 - 07:00',
+        accessor: '13'
+    },
+    {
+        Header: '07:00 - 07:30',
+        accessor: '14'
+    },
+    {
+        Header: '07:30 - 08:00',
+        accessor: '15'
+    },
+    {
+        Header: '08:00 - 08:30',
+        accessor: '16'
+    },
+    {
+        Header: '08:30 - 09:00',
+        accessor: '17'
+    },
+    {
+        Header: '09:00 - 09:30',
+        accessor: '18'
+    },
+    {
+        Header: '09:30 - 10:00',
+        accessor: '19'
+    },
+    {
+        Header: '10:00 - 10:30',
+        accessor: '20'
+    },
+    {
+        Header: '10:30 - 11:00',
+        accessor: '21'
+    },
+    {
+        Header: '11:00 - 11:30',
+        accessor: '22'
+    },
+    {
+        Header: '11:30 - 12:00',
+        accessor: '23'
+    },
+    {
+        Header: '012:00 - 12:30',
+        accessor: '24'
+    },
+    {
+        Header: '12:30 - 13:00',
+        accessor: '25'
+    },
+    {
+        Header: '13:00 - 13:30',
+        accessor: '26'
+    },
+    {
+        Header: '13:30 - 14:00',
+        accessor: '27'
+    },
+    {
+        Header: '14:00 - 14:30',
+        accessor: '28'
+    },
+    {
+        Header: '14:30 - 15:00',
+        accessor: '29'
+    },
+    {
+        Header: '15:00 - 15:30',
+        accessor: '30'
+    },
+    {
+        Header: '15:30 - 16:00',
+        accessor: '31'
+    },
+    {
+        Header: '16:00 - 16:30',
+        accessor: '32'
+    },
+    {
+        Header: '16:30 - 17:00',
+        accessor: '33'
+    },
+    {
+        Header: '17:00 - 17:30',
+        accessor: '34'
+    },
+    {
+        Header: '17:30 - 18:00',
+        accessor: '35'
+    },
+    {
+        Header: '18:00 - 18:30',
+        accessor: '36'
+    },
+    {
+        Header: '18:30 - 19:00',
+        accessor: '37'
+    },
+    {
+        Header: '19:00 - 19:30',
+        accessor: '38'
+    },
+    {
+        Header: '19:30 - 20:00',
+        accessor: '39'
+    },
+    {
+        Header: '20:00 - 20:30',
+        accessor: '40'
+    },
+    {
+        Header: '20:30 - 21:00',
+        accessor: '41'
+    },
+    {
+        Header: '21:00 - 21:30',
+        accessor: '42'
+    },
+    {
+        Header: '21:30 - 22:00',
+        accessor: '43'
+    },
+    {
+        Header: '22:00 - 22:30',
+        accessor: '44'
+    },
+    {
+        Header: '22:30 - 23:00',
+        accessor: '45'
+    },
+    {
+        Header: '23:00 - 23:30',
+        accessor: '46'
+    },
+    {
+        Header: '23:30 - 24:00',
+        accessor: '47'
     },
 ]
