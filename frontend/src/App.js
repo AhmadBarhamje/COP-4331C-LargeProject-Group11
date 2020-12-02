83fadd1f93be1403b5e089b2533a9e881fcf569f
mzyousaf <mzainyousaf.01@gmail.com>
2020-12-02 15:42:41 +0500

    schedual

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
@@ -7,7 +8,7 @@ import CardPage from './pages/CardPage';
 import RegisterPage from './pages/RegisterPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import ChangePasswordPage from './pages/ChangePasswordPage';
-
+import Schedual from './pages/Schedual'
 function App() {
   return (
     <Router>
@@ -26,6 +27,9 @@ function App() {
         </Route>
         <Route path="/cards" exact>
           <CardPage/>
+        </Route>
+        <Route path="/schedual" exact>
+          <Schedual/>
         </Route>
         <Redirect to="/"/>
       </Switch>
