bdff927a3dd271869e671c889615eb2b9e50cb52
samsannchez <69266761+samsannchez@users.noreply.github.com>
2020-12-01 20:58:51 -0500

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -90,7 +90,7 @@ function Register()
                 <input type="text" id="firstName" class="fadeIn second" placeholder="First Name" ref={(c) => firstName = c} required/><br />
                 <input type="text" id="lastName" class="fadeIn second" placeholder="Last name" ref={(c) => lastName = c} /><br />
                 <input type="text" id="registerUsername" class="fadeIn second" placeholder="Username" ref={(c) => registerUsername = c} required/><br />
-                <input type="email"  id="registerEmail" class="fadeIn third" placeholder="Email" ref={(c) => registerEmail = c} required/><br />
+                <input type="text"  id="registerEmail" class="fadeIn third" placeholder="Email" ref={(c) => registerEmail = c} required/><br />
                 <input type="password"  id="registerPassword" class="fadeIn third" placeholder="Password" ref={(c) => registerPassword = c} required/><br />
                 <input type="password"  id="registerPasswordCheck" class="fadeIn third" placeholder="Confirm Password" ref={(c) => registerPasswordCheck = c} required/><br />
                 <input type="submit" id="registerButton" className="buttons" value = "Register" onClick={doRegister} />
