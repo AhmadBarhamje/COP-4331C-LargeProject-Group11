cd01ac589f240e6af5f908c454e14fc3c0df33a6
samsannchez <69266761+samsannchez@users.noreply.github.com>
2020-12-02 14:48:18 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 import RegisterPage from './pages/RegisterPage';
 import UpdatePage from './pages/UpdatePage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import ChangePasswordPage from './pages/ChangePasswordPage';
 
 function App() {
   return (
@@ -19,6 +21,12 @@ function App() {
         </Route>
         <Route path="/cards" exact>
           <CardPage/>
+        </Route>
+        <Route path='/forgotpassword' exact>
+          <ForgotPasswordPage/>
+        </Route>
+        <Route path='/changepassword' exact>
+          <ChangePasswordPage/>
         </Route>
         <Route path="/update" exact>
           <UpdatePage/>
